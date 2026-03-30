8aa9b6d437ae9477d32f5c24bb2ce494cf9c2c43
shivamburkul <shivamburkul1179@gmail.com>
2026-03-30 17:57:00 +0530

    Practicing DB

--- lib/data/local/db_helper.dart
+++ lib/data/local/db_helper.dart
@@ -1,5 +1,20 @@
+import 'package:sqflite/sqflite.dart';
+
 class DBHelper {
   DBHelper._();
 
   static final DBHelper getInstance = DBHelper._();
+
+  Database? myDB;
+
+  Database getDB() {
+    if(myDB!=null){
+      return myDB!;
+    } else {
+      myDB = openDB();
+      return myDB!;
+    }
+  }
+
+  openDB() {}
 }
